ddb6a292debf25f2402005d983ddf6f29aacbe61
Erik Abair <erik.abair@gmail.com>
2025-08-14 20:50:38 -0700

    Exposes NO_OUTPUT at top level.

--- src/producer_graph/__init__.py
+++ src/producer_graph/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 # ruff: noqa: F401 imported but unused; consider removing, adding to `__all__`, or using a redundant alias
 from producer_graph._pipeline import Pipeline, PipelineNode
 from producer_graph._processor import (
+    NO_OUTPUT,
     BasicTransformCallable,
     BatchingCallable,
     BatchingProcessor,
